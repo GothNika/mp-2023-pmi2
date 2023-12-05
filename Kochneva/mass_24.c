e17b00946b257b97a98f77126b03cdec6000123d
klubNika <146868899+GothNika@users.noreply.github.com>
2023-12-05 21:37:23 +0300

    Update mass_24.c
    исправила и использовала доп массив

--- Kochneva/mass_24.c
+++ Kochneva/mass_24.c
@@ -1,41 +1,35 @@
 #include <stdio.h>
 #include <locale.h>
-int main()
-{
-    setlocale(LC_ALL, "Russian");
-    int a[5], i, j, k, flag = 0, duplicates_found = 0;
 
-    for (i = 0; i < 5; i++)
-    {
+int main() {
+    setlocale(LC_ALL, "Rus");
+    int a[5], duplicate_flags[5] = { 0, 0, 0, 0, 0 };
+    int i, j, duplicates_found = 0;
+
+    for (i = 0; i < 5; i++) {
         scanf_s("%d", &a[i]);
     }
 
-    for (i = 0; i < 5; i++)
-    {
-        flag = 0;
-        for (j = i + 1; j < 5; j++)
-        {
-            if (a[i] == a[j])
-            {
-                flag = 1;
-            }
-        }
-        if (flag == 1)
-        {
-            for (k = 0; k < i; k++)
-            {
-                if (a[i] == a[k])
-                    flag = 0;
-            }
-            if (flag == 1)
-            {
-                printf("Обнаружены одинаковые элементы. Повторяющееся значение: %d\n", a[i]);
-                duplicates_found = 1;
+    for (i = 0; i < 5; i++) {
+        for (j = i + 1; j < 5; j++) {
+            if (a[i] == a[j]) {
+                duplicate_flags[i] = 1;
+                duplicate_flags[j] = 1;
             }
         }
     }
-    if (duplicates_found == 0)
+
+    printf("Найденные дубликаты: ");
+    for (i = 0; i < 5; i++) {
+        if (duplicate_flags[i] == 1) {
+            printf("%d ", a[i]);
+            duplicates_found = 1;
+        }
+    }
+
+    if (duplicates_found == 0) {
         printf("Одинаковых элементов не обнаружено");
+    }
 
     return 0;
 }
